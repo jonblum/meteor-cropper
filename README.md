66feed4ad0d78ae246d96090e048862d67656347
Jonathan Elliott Blum <jon@jonblum.net>
2014-11-12 10:28:21 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ meteor-cropper
 ==============
 
 Fengyuan Chen's terrific Cropper jQuery plugin, packaged for Meteor
+
+## How to install
+
+`meteor add jonblum:jquery-cropper'
+
+## Demos and Docs
+
+http://fengyuanchen.github.io/cropper/
---
+++ package.js
@@ -0,0 +1,17 @@
+Package.describe({
+  name: 'jonblum:meteor-cropper',
+  summary: 'Fengyuan Chen's terrific Cropper jQuery plugin, packaged for Meteor',
+  version: '0.7.2',
+  git: 'https://github.com/jonblum/meteor-cropper'
+});
+
+Package.onUse(function (api) {
+  api.versionsFrom('0.9.0');
+  api.use('jquery', 'client');
+  api.imply('jquery', 'client');
+  api.addFiles([
+    'cropper/src/cropper.js',
+    'cropper/src/cropper.css',
+  ], 'client'
+  );
+});
