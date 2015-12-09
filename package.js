f233497bb36db7c746a798ee9e246b2045a0a8dc
Jonathan Elliott Blum <jon@jonblum.net>
2015-12-09 10:52:06 -0500

    v2.2.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "jonblum:jquery-cropper",
   summary: "Fengyuan Chen's terrific jQuery plugin for cropping/zooming/rotating images, packaged for Meteor",
-  version: "2.1.0",
+  version: "2.2.0",
   git: "https://github.com/jonblum/meteor-cropper.git"
 });
 
